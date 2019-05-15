21ed9342b7046ae33b6c7c7a27bce09c8127bb1c
damianjnc <damianjnc@gmail.com>
2019-05-15 09:44:58 +0100

    animated modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React from "react";
+import Transition from "react-transition-group/Transition";
 
-import './Modal.css';
+import "./Modal.css";
 
-const modal = (props) => {
-    const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed'];
-    return(
-        <div className={cssClasses.join(' ')}>
+const modal = props => {
+  return (
+    <Transition mountOnEnter unmountOnExit in={props.show} timeout={1000}>
+      {state => {
+        const cssClasses = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModalClosed"
+            : null
+        ];
+        return (
+          <div className={cssClasses.join(" ")}>
             <h1>A Modal</h1>
-            <button className="Button" onClick={props.closeModal}>Dismiss</button>
-        </div>
-    );
-
-}
+            <button className="Button" onClick={props.closeModal}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
+  );
+};
 
 export default modal;
